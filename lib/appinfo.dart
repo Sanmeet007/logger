8fa5dbe3e08f4efc0277078857186ac250d3f028
Sanmeet007 <ssanmeet123@gmail.com>
2024-04-03 02:27:50 +0530

    bug fix

--- lib/appinfo.dart
+++ lib/appinfo.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppInfo extends StatelessWidget {
-  const AppInfo({Key? key}) : super(key: key);
+  const AppInfo({super.key});
 
   @override
   Widget build(BuildContext context) {
