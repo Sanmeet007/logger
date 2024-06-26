9090996bc2bb1d84634f8588ea65431bb7ffc1a0
Sanmeet007 <ssanmeet123@gmail.com>
2024-06-27 01:31:07 +0530

    removed shared prefs :no use

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'app_interface.dart';
 import 'theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
 
   runApp(
     MaterialApp(
