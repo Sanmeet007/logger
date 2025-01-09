1910bd6188b749ffc6ffb0cef818eadfd1a021fa
Sanmeet007 <ssanmeet123@gmail.com>
2025-01-09 15:18:44 +0530

    fixed colors

--- lib/components/divider.dart
+++ lib/components/divider.dart
@@ -7,7 +7,7 @@ class LogDivider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Divider(
       color: Theme.of(context).brightness == Brightness.dark
-          ? const Color.fromARGB(255, 15, 15, 15)
+          ? const Color.fromARGB(121, 16, 16, 16)
           : const Color.fromARGB(201, 233, 216, 255),
       height: 1.0,
     );
