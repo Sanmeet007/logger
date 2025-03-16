994c1458788fa982cf8c5a5c6672e3835433ba3d
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-16 21:17:08 +0530

    added riverpod initializer

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/providers/shared_utility_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'core/app_core.dart';
 import 'themeing/theme.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  FlutterNativeSplash.remove();
 
   runApp(
-    MaterialApp(
-      title: "Logger",
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: ThemeClass.lightTheme,
-      darkTheme: ThemeClass.darkTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: const [
-        Locale('en'),
-        ...AppLocalizations.supportedLocales,
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
       ],
-      home: AppCore(
-        widgetsBinding: widgetsBinding,
+      child: MaterialApp(
+        title: "Logger",
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        theme: ThemeClass.lightTheme,
+        darkTheme: ThemeClass.darkTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: const [
+          Locale('en'),
+          ...AppLocalizations.supportedLocales,
+        ],
+        home: AppCore(
+          widgetsBinding: widgetsBinding,
+        ),
       ),
     ),
   );
