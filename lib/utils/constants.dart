dd0fe895b1efd59968adbbda2ef6c04060f28b9f
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-16 21:16:43 +0530

    added constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+const String sharedOnboardingKey = "show-onboarding";
+const String sharedPhoneAccountIdFilteringKey = "phone_acc_id_filtering";
+const String sharedDurationFilteringKey = "duration_filtering";
+const String sharedConfirmDownloadKey = "confirm_download";
+const String sharedLogsSharingKey = "sharing";
+const String sharedDisplayCallLogCountKey = "call_log_count";
+const String sharedImportTypeKey = "import_type";
+const String sharedExportFileNameFormatKey = "exported_filename_format";
+const String sharedDisplayTotalCallDurationKey = "show_total_call_duration";
