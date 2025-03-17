568b60cf3b0ef39714345e81ac352ed9d454d380
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-17 15:27:51 +0530

    added current call logs provider

---
+++ lib/providers/current_call_logs_provider.dart
@@ -0,0 +1,42 @@
+import 'package:call_log/call_log.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/providers/call_logs_provider.dart';
+
+class CurrentCallLogsNotifier extends StateNotifier<Iterable<CallLogEntry>> {
+  final Ref ref;
+
+  CurrentCallLogsNotifier(this.ref) : super(Iterable.empty()) {
+    _initialize();
+  }
+
+  void _initialize() {
+    final asyncCallLogs = ref.watch(callLogsNotifierProvider);
+    if (asyncCallLogs.hasValue) {
+      state = asyncCallLogs.value ?? Iterable.empty();
+    }
+  }
+
+  void update(Iterable<CallLogEntry> entries) {
+    state = entries;
+  }
+
+  List<String> getAvailablePhoneAccountIds() {
+    final uniquePhoneAccountIds = <String>{"Any"};
+    for (var entry in state) {
+      uniquePhoneAccountIds.add(entry.phoneAccountId ?? "Unknown");
+    }
+    return uniquePhoneAccountIds.toList();
+  }
+
+  void reset() {
+    final asyncCallLogs = ref.read(callLogsNotifierProvider);
+    if (asyncCallLogs.hasValue) {
+      state = asyncCallLogs.value ?? Iterable.empty();
+    }
+  }
+}
+
+final currentCallLogsNotifierProvider =
+    StateNotifierProvider<CurrentCallLogsNotifier, Iterable<CallLogEntry>>(
+  CurrentCallLogsNotifier.new,
+);
