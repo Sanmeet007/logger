12841f96dfc5d72313adfa73b465e222040060ee
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-19 23:40:02 +0530

    used constants instead of bare strings

--- lib/core/log_filters.dart
+++ lib/core/log_filters.dart
@@ -10,6 +10,7 @@ import 'package:logger/providers/log_filters_provider.dart';
 import 'package:logger/utils/filter_date_ranges.dart';
 import 'package:logger/utils/filters.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:logger/utils/constants.dart' as constants;
 
 class LogFilters extends ConsumerStatefulWidget {
   final WidgetRef parentRef;
@@ -343,7 +344,7 @@ class _LogFiltersState extends ConsumerState<LogFilters> {
                                     (item) => DropdownMenuItem(
                                       value: item,
                                       child: Text(
-                                        item == "Any"
+                                        item == constants.defaultPhoneAccountId
                                             ? AppLocalizations.of(context)
                                                 .anyText
                                             : item,
