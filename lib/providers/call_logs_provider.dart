bb6cf06b8980d8535292ca2d619595c61026038a
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-19 20:07:34 +0530

    cached values

--- lib/providers/call_logs_provider.dart
+++ lib/providers/call_logs_provider.dart
@@ -2,6 +2,8 @@ import 'package:call_log/call_log.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class CallLogsNotifier extends AsyncNotifier<Iterable<CallLogEntry>> {
+  static List<String>? _cachedAccountIds;
+
   @override
   FutureOr<Iterable<CallLogEntry>> build() async {
     return await CallLog.get();
@@ -14,12 +16,18 @@ class CallLogsNotifier extends AsyncNotifier<Iterable<CallLogEntry>> {
   }
 
   List<String> getAvailablePhoneAccountIds() {
+    if (_cachedAccountIds != null) {
+      return _cachedAccountIds!;
+    }
+
     if (state.value != null) {
       final uniquePhoneAccountIds = <String>{"Any"};
       for (var entry in state.value!) {
         uniquePhoneAccountIds.add(entry.phoneAccountId ?? "Unknown");
       }
-      return uniquePhoneAccountIds.toList();
+
+      _cachedAccountIds = uniquePhoneAccountIds.toList();
+      return _cachedAccountIds!;
     } else {
       return List.empty();
     }
